f5d9fd47e7cb4625a3548b56d8dcbb58cc65f33e
Lewy <amatodominik@gmail.com>
2026-06-16 11:27:26 +0200

    ...

--- Main.java
+++ Main.java
@@ -1,3 +1,16 @@
+/*
+ * Main.java - Einstiegspunkt des Spiels
+ * 
+ * Diese Klasse ist der Startpunkt der Anwendung und muss zuerst ausgeführt werden.
+ * Sie initialisiert die GameUI (das Hauptmenü mit Schwierigkeitsauswahl) auf dem Event Dispatch Thread.
+ * 
+ * Ablauf:
+ * 1. Main.java wird ausgeführt und startet die GameUI
+ * 2. Der Spieler sieht das Hauptmenü und wählt einen Schwierigkeitsgrad
+ * 3. Nachdem der Spieler auf "Start Game" klickt, wird MainGame.java gestartet
+ * 4. MainGame.java lädt das eigentliche Spiel mit dem Generator
+ */
+
 import javax.swing.*;
 
 public class Main {
@@ -5,3 +18,5 @@ public class Main {
         SwingUtilities.invokeLater(GameUI::new);
     }
 }
+
+//
